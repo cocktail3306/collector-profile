53a491001d1fa498c30fa8a706f31a6f5439d197
eric fournier <eric.fournier.06@gmail.com>
2026-01-03 15:25:58 +0100

    Inital commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import AbstractProvider from "@/component/AbstractProvider";
+import { WagmiProvider } from 'wagmi';
+import { config } from "@/lib/wagmi"; 
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,11 +27,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <WagmiProvider config={config}>
+        <AbstractProvider>
+          <body
+//            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          >
+            {children}
+          </body>
+        </AbstractProvider>
+      </WagmiProvider>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,85 @@
-import Image from "next/image";
+"use client";
+
+import 'dotenv/config';
+import { Alchemy, Network } from 'alchemy-sdk';
+import { useAccount } from 'wagmi';
+import { useState, useEffect } from 'react';
+import { useLoginWithAbstract } from "@abstract-foundation/agw-react";
+import { checkTraitCompleteness } from "@/lib/traitMatchingService";
+import styles from '@/styles/NFTCard.module.css';
 
 export default function Home() {
+  const { login, logout} = useLoginWithAbstract();
+  const config = {
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    network: Network.ABSTRACT_MAINNET,
+  };
+  const alchemy = new Alchemy(config);
+
+  const { address, isConnected } = useAccount();
+  const [nfts, setNfts] = useState<any>(null);
+  const [completeness, setCompleteness] = useState<{ traitType: string; hasAll: boolean }[]>([]);
+
+  useEffect(() => {
+    if (isConnected && address) {
+      checkTraitCompleteness(address).then(({ nfts, completeness }) => {
+        setNfts(nfts);
+        setCompleteness(completeness);
+      });
+    }
+  }, [address, isConnected]);
+
+  console.log('apiKey: ' + process.env.API_KEY)
+  console.log('CA: ' + process.env.CONTRACT_ADDRESS)
+
+  console.log("NFTs raw:", nfts);
+
+  if (!isConnected)
+    return (
+      <div>
+        <button onClick={login}>Connect</button>
+      </div>
+    );
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div>
+      <button onClick={logout}>Disconnect</button>
+      <p>Connected to: {address}</p>
+
+      <h2>Your Characters</h2>
+      {nfts?.length === 0 ? (
+        <p>You don't own any NFT from this collection.</p>
+      ) : (
+        <>
+          {completeness?.map(
+            ({ traitType, hasAll }) =>
+              hasAll && (
+                <p key={traitType} className={styles.complete}>
+                  You got all the possible values for the {traitType} trait
+                </p>
+              )
+          )}
+
+          {nfts?.map((nft: any, i: number) => (
+            <div key={nft.tokenId} className={styles.card}>
+              <div className={styles.image}>
+                <img src={nft.image.thumbnailUrl} alt={nft.name} />
+              </div>
+              <div className={styles.details}>
+                <h3>{nft.name}</h3>
+                <h4>Attributs :</h4>
+                <ul>
+                  {nft.raw.metadata.attributes.map((attr: any, j: number) => (
+                    <li key={i + '_' + j}>
+                      <strong>{attr.trait_type} :</strong> {attr.value}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 }
